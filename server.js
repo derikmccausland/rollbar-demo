88f8f11eed4756b4df8aa4ecb1abba392f77b290
Derik McCausland <derikmccausland@gmail.com>
2022-07-21 12:37:23 -0600

    added functions

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ rollbar.log('Hello world!')
 const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.get('/', (req, res) => {
+    rollbar.info('someone launched your html')
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
@@ -37,11 +38,14 @@ app.post('/api/students', (req, res) => {
 
    try {
        if (index === -1 && name !== '') {
+           rollbar.log('students successfully added', {author: 'Derik', type: 'manual entry'})
            students.push(name)
            res.status(200).send(students)
        } else if (name === ''){
+        rollbar.error ("no name provided")
            res.status(400).send('You must enter a name.')
        } else {
+        rollbar.error("student already exists")
            res.status(400).send('That student already exists.')
        }
    } catch (err) {
@@ -53,6 +57,7 @@ app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
     
     students.splice(targetIndex, 1)
+    rollbar.info('student was deleted')
     res.status(200).send(students)
 })
 
